166e1c25222dec9272cff15dc594a43313e69f2b
Zubair <theabyss@umich.edu>
2012-06-15 08:17:04 -0400

    Base URL Insert
    Signed-off-by: Zubair <theabyss@umich.edu>

--- application/config/config.php
+++ application/config/config.php
@@ -8,13 +8,13 @@
 | URL to your CodeIgniter root. Typically this will be your base URL,
 | WITH a trailing slash:
 |
-|	http://example.com/
+|	http://zubairahsan.com/
 |
 | If this is not set then CodeIgniter will guess the protocol, domain and
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://zubairahsan.com/';
 
 /*
 |--------------------------------------------------------------------------
